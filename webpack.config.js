105d336703b7b0999a36d4b9702004408f637b8e
NuclearPhoenixx <nuclearphoenix@public-files.de>
2024-02-23 14:26:42 +0100

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,39 @@
-const webpack = require("webpack")
-const path = require('path')
+const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-    entry    : {
-        PolynomialRegression: './src/PolynomialRegression.js'
-    },
-    output   : {
-        path    : path.resolve(__dirname, 'dist/'),
-        filename: '[name].js',
-        libraryTarget : "umd"
-    },
-    module  : {
-        rules : [
-            {
-                test    : /\.js?$/,
-                exclude : /node_modules/,
-                use     : {
-                    loader  : 'babel-loader',
-                    options : {
-                        presets : [
-                            ['@babel/env', {
-                                targets : {
-                                    ie : 11
-                                }
-                            }]
-                        ]
-                    }
-                }
-            }
-        ]
-    },
-    devtool: process.env.NODE_ENV === "development" ? "inline-source-map" : false
-}
+  entry: {
+    PolynomialRegression: './src/PolynomialRegression.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/env',
+                {
+                  targets: {
+                    ie: 11,
+                  },
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  mode: 'production',
+  devtool: 'source-map',
+};
